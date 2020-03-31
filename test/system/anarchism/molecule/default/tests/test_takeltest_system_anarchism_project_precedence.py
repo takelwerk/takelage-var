ccce514dacf8c57d19e0e88173a8acd26cfdc067
Stephan Wehrheim <swehrheim@geospin.de>
2020-03-31 12:33:01 +0200

    Fix precedence test

--- test/system/anarchism/molecule/default/tests/test_takeltest_system_anarchism_project_precedence.py
+++ test/system/anarchism/molecule/default/tests/test_takeltest_system_anarchism_project_precedence.py
@@ -8,5 +8,5 @@ def test_takeltest_system_precedence_can_read_role_default(host, testvars):
 
 
 def test_takeltest_system_precedence_project_over_role_default(host, testvars):
-    expected = ['anarchism', 'fortune-anarchism']
+    expected = ['anarchism', 'fortune-anarchism', 'sudo']
     assert testvars['takel_anarchism_deb_install_packages'] == expected
