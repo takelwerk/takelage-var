8b33e1f79dc89e69657b6200235ce26b312ef3c2
Stephan Wehrheim <swehrheim@geospin.de>
2020-04-09 11:23:31 +0000

    Update takel-anarchism

--- test/system/anarchism/roles/takel-anarchism/molecule/default/system/test_takel-anarchism_system.py
+++ test/system/anarchism/roles/takel-anarchism/molecule/default/system/test_takel-anarchism_system.py
@@ -6,5 +6,5 @@ testinfra_hosts = takeltest.hosts()
 def test_takel_anarchism_system_fortune_greeting(host, testvars):
     if 'fortune-anarchism' in testvars['takel_anarchism_deb_install_packages']:
         with host.sudo():
-            output = host.check_output('. /root/.bashrc')
+            output = host.check_output('bash -c ". /root/.bashrc"')
             assert '---+-' in output
