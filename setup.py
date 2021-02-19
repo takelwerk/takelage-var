3417cada827a9c5adc647a57d09c8ddb3f94ec67
Stephan Wehrheim <swehrheim@geospin.de>
2021-02-19 15:55:22 +0100

    New version 0.7.6: install cryptography

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='takeltest',
-        version='0.7.5',
+        version='0.7.6',
         author='Geospin',
         author_email='takelage@geospin.de',
         description='Fixtures for ansible, testinfra and molecule',
