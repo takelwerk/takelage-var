b27f39ef337fc6200397f2ea1a4081c808f9e457
Stephan Wehrheim <swehrheim@geospin.de>
2021-03-25 14:11:06 +0100

    Fix Rakefile

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='takeltest',
-        version='0.7.24',
+        version='0.7.25',
         author='Geospin',
         author_email='takelage@geospin.de',
         description='Fixtures for ansible, testinfra and molecule',
