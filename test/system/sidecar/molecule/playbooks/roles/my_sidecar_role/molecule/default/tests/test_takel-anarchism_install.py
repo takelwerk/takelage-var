e590e07692ba2672efde9467915c96e35e0cff1a
Stephan Wehrheim <swehrheim@geospin.de>
2021-03-26 00:42:23 +0100

    Fix typo

--- test/system/sidecar/molecule/playbooks/roles/my_sidecar_role/molecule/default/tests/test_takel-anarchism_install.py
+++ test/system/sidecar/molecule/playbooks/roles/my_sidecar_role/molecule/default/tests/test_takel-anarchism_install.py
@@ -3,7 +3,7 @@ import takeltest
 testinfra_hosts = takeltest.hosts()
 
 
-def test_my_sidecar_role_default_valiu(host, testvars):
+def test_my_sidecar_role_default_variable(host, testvars):
     default_value = testvars['my_sidecar_role_default_key']
 
     assert 'my_default_value' == default_value
