b55f0e26e99dacd985b94d2acb3fbbe3f16b834e
Stephan Wehrheim <swehrheim@geospin.de>
2021-03-25 02:32:20 +0100

    Fix github workflows

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='takeltest',
-        version='0.7.22',
+        version='0.7.23',
         author='Geospin',
         author_email='takelage@geospin.de',
         description='Fixtures for ansible, testinfra and molecule',
